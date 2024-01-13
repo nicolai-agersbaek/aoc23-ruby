f2f7f5a7f8cafa9616c148c09a8317b52c99834a
Nicolai Agersbæk <nicolai.agersbaek@gmail.com>
2024-01-13 12:48:06 +0100

    Added initial bundle gem files.

---
+++ lib/aoc23.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aoc23/version"
+
+module Aoc23
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aoc23/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Aoc23
+  VERSION = "0.1.0"
+end
