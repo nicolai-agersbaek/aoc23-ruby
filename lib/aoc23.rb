e9c1cfa0e6ecc01c37ae3d152399d0b569804658
Nicolai Agersbæk <niag@clearhaus.com>
2024-01-15 15:39:38 +0100

    Stuff.

--- lib/aoc23.rb
+++ lib/aoc23.rb
@@ -4,18 +4,27 @@ require_relative "aoc23/version"
 require_relative "aoc23/cli/args_parser"
 require_relative "aoc23/cli/arg_def"
 require_relative "aoc23/cli/option_def"
+require_relative "aoc23/cli/option_def"
 
 module Aoc23
   class Error < StandardError; end
   
   parser = Cli::ArgsParser.new(
     arg_defs: [
-      Cli::ArgDef.new(name: "task_num", cls: Integer),
-      Cli::ArgDef.new(name: "input", cls: String),
+      Cli::ArgDef.new(name: "day", cls: Integer),
+      # Cli::ArgDef.new(name: "input", cls: String),
     ],
     option_defs: [],
   )
 
-  printf "Command-line args: %s\n", parser.parse
+  day = parser.parse.fetch("day")
+
+  input = File.read("data/day#{day}.txt").split("\n")
+
+  require_relative "aoc23/solutions/day#{day}"
+
+  solution = Solutions::Day1.new.solve(input: input)
+
+  printf "Solution: %s\n", solution
 
 end
